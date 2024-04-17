05a1bcf57ab1357ce77af53d00b3da2ddb11a3d5
saularraffi <saular.raffi@airspacelink.com>
2024-04-17 17:31:45 -0400

    commit

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("hello")
+import requests
+import os
+
+GH_TOKEN = command_output = os.popen('gh auth token').read().strip()
+GH_GRAPHQL_URL = 'https://api.github.com/graphql'
+
+query='''
+query {
+  repository(owner: \"saularraffi\", name: \"test\") {
+    object(expression: \"mybranch\") {
+      ... on Commit {
+        blame(path: \"README.md\") {
+          ranges {
+            commit {
+              committedDate
+              author {
+                name
+                email
+              }
+            }
+            startingLine
+            endingLine
+          }
+        }
+      }
+    }
+  }
+}
+'''
+
+response = requests.post(
+    GH_GRAPHQL_URL,
+    headers={'Authorization': f'bearer {GH_TOKEN}'},
+    json={'query': query}
+)
+
+print(response.json())
